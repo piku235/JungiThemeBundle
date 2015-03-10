b2adf53ff0e1c7df3466d5ade5a5756aa9b81194
Piotr Kugla <piku235@gmail.com>
2015-03-10 20:03:37 +0100

    fixed a small bug in the TemplateNameParser

--- Templating/TemplateNameParser.php
+++ Templating/TemplateNameParser.php
@@ -66,7 +66,7 @@ class TemplateNameParser extends BaseTemplateNameParser
         }
 
         if ($theme instanceof VirtualThemeInterface) {
-            $reference = new VirtualTemplateReference($parent, $theme, $theme->getPointedTheme());
+            $reference = new VirtualTemplateReference($parent, $theme->getName(), $theme->getPointedTheme()->getName());
         } else {
             $reference = new TemplateReference($parent, $theme->getName());
         }
