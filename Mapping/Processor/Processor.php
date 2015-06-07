92e93b77be978d0b7ce86080e25952f34ddac022
piku235 <piku235@gmail.com>
2015-06-07 15:14:10 +0200

    removed processor workers, merged them into the Processor class

--- Mapping/Processor/Processor.php
+++ Mapping/Processor/Processor.php
@@ -11,7 +11,10 @@
 
 namespace Jungi\Bundle\ThemeBundle\Mapping\Processor;
 
+use Jungi\Bundle\ThemeBundle\Mapping\ContainerInterface;
+use Jungi\Bundle\ThemeBundle\Mapping\StandardThemeDefinition;
 use Jungi\Bundle\ThemeBundle\Mapping\ThemeDefinitionRegistryInterface;
+use Jungi\Bundle\ThemeBundle\Mapping\VirtualThemeDefinition;
 
 /**
  * Processor.
@@ -21,27 +24,122 @@ use Jungi\Bundle\ThemeBundle\Mapping\ThemeDefinitionRegistryInterface;
 class Processor implements ProcessorInterface
 {
     /**
-     * @var ProcessorInterface[]
-     */
-    private $workers = array();
-
-    /**
-     * Adds a processor worker.
-     *
-     * @param WorkerInterface $worker A worker
-     */
-    public function addWorker(WorkerInterface $worker)
-    {
-        $this->workers[] = $worker;
-    }
-
-    /**
      * {@inheritdoc}
      */
     public function process(ThemeDefinitionRegistryInterface $registry)
     {
-        foreach ($this->workers as $worker) {
-            $worker->process($registry);
+        $this->processParameters($registry);
+        $this->processVirtualThemes($registry);
+    }
+
+    /**
+     * Processes virtual theme definitions
+     *
+     * @param ThemeDefinitionRegistryInterface $registry
+     */
+    private function processVirtualThemes(ThemeDefinitionRegistryInterface $registry)
+    {
+        foreach ($registry->getThemeDefinitions() as $themeName => $theme) {
+            if (!$theme instanceof VirtualThemeDefinition) {
+                continue;
+            }
+
+            // Move referenced theme definitions to a corresponding virtual theme
+            foreach ($theme->getThemeReferences() as $referencedTheme) {
+                $theme->addTheme($referencedTheme, $registry->getThemeDefinition($referencedTheme));
+
+                // As a theme belongs now to the virtual theme we do not need it in the registry
+                $registry->removeThemeDefinition($referencedTheme);
+            }
+
+            // Validate
+            foreach ($theme->getThemes() as $childTheme) {
+                if ($childTheme instanceof VirtualThemeDefinition) {
+                    throw new \LogicException(sprintf(
+                        'Virtual themes cannot consists of another virtual themes. Encountered at the theme "%s".',
+                        $themeName
+                    ));
+                }
+            }
         }
+    }
+
+    /**
+     * Processes parameters contained in a registry
+     *
+     * @param ThemeDefinitionRegistryInterface $registry
+     */
+    private function processParameters(ThemeDefinitionRegistryInterface $registry)
+    {
+        if (!$registry instanceof ContainerInterface || !$registry->getParameters()) {
+            return;
+        }
+
+        foreach ($registry->getThemeDefinitions() as $theme) {
+            if ($theme instanceof StandardThemeDefinition) {
+                $theme->setPath($this->resolveParameter($theme->getPath(), $registry));
+            }
+
+            // Tags
+            foreach ($theme->getTags() as $tag) {
+                $args = $tag->getArguments();
+                foreach ($args as &$arg) {
+                    $arg = $this->resolveParameter($arg, $registry);
+                }
+                $tag->setArguments($args);
+            }
+
+            // ThemeInfo
+            if (null !== $info = $theme->getInformation()) {
+                $info->setProperties($this->resolveParameterRecursive($info->getProperties(), $registry));
+            }
+        }
+    }
+
+    /**
+     * Resolves a parameter value in a recursive way if present.
+     *
+     * @param mixed              $value     A value
+     * @param ContainerInterface $container A container
+     *
+     * @return mixed
+     *
+     * @throws \RuntimeException When the given parameter does not exist
+     */
+    private function resolveParameterRecursive($value, ContainerInterface $container)
+    {
+        if (is_array($value)) {
+            foreach ($value as &$child) {
+                $child = $this->resolveParameterRecursive($child, $container);
+            }
+
+            return $value;
+        }
+
+        return $this->resolveParameter($value, $container);
+    }
+
+    /**
+     * Resolves a parameter value if present.
+     *
+     * @param string             $value     A value
+     * @param ContainerInterface $container A container
+     *
+     * @return mixed
+     *
+     * @throws \RuntimeException When the given parameter does not exist
+     */
+    private function resolveParameter($value, ContainerInterface $container)
+    {
+        if (!preg_match('/^%([^\s%]+)%$/', $value, $matches)) {
+            return $value;
+        }
+
+        $paramName = $matches[1];
+        if (!$container->hasParameter($paramName)) {
+            throw new \RuntimeException(sprintf('The parameter "%s" can not be found.', $paramName));
+        }
+
+        return $container->getParameter($paramName);
     }
 }
